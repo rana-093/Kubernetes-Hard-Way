fca061cda6e6caa3202c6c7b73119589d7608241
RanaM <rana_093@yahoo.com>
2023-12-25 22:31:33 +0600

    Added src code to test on k8s cluster

---
+++ src/app/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mysql = require('mysql');
+require('dotenv').config();
+
+const app = express();
+const port = 3000;
+
+// MySQL configuration
+const dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+};
+
+// Create MySQL connection pool
+const pool = mysql.createPool(dbConfig);
+
+// Fetch employees from the database
+app.get('/employees', (req, res) => {
+    pool.query('SELECT * FROM employee', (error, results) => {
+        if (error) throw error;
+        res.json(results);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
